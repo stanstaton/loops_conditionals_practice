e188a53262a3f337ccc93dc7878da419b540b10b
stanstaton <thatkidenzo@gmail.com>
2019-07-25 11:36:18 -0700

    un-commented out some code, loops go to designated amounts

--- forloops.js
+++ forloops.js
@@ -1,16 +1,16 @@
-// for (var i = 0; i <= 100; i++) {
-// 	console.log(i);
-// }
+for (var i = 0; i <= 100; i++) {
+	console.log(i);
+}
 
-// for (var j = 7; j <= 635; j++) {
-// 	console.log(j);
-// }
+for (var j = 7; j <= 635; j++) {
+	console.log(j);
+}
 
-// var LIMIT = 100
+var LIMIT = 100
 
-// for (var start = 0; start <= LIMIT; start++) {
-// 	console.log(start);
-// }
+for (var start = 0; start <= LIMIT; start++) {
+	console.log(start);
+}
 
 var busRouteEnds = 24
 
--- whileloops.js
+++ whileloops.js
@@ -1,20 +1,20 @@
 var x = 0;
 
-while (x<10) {
+while (x<1000) {
 	console.log("Ginger chocolate honey patties");
 	x++;
 }
 
 var i = 0;
 
-while (i <=10) {
+while (i <=1000) {
 	console.log("=>",i);
 	i++;
 }
 
 var k = 0;
 
-while (k<=10) {
+while (k<=1000) {
 	console.log(`Current loop number is: ${k}`)
 	k++
 }
