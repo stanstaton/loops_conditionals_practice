5379a82eb582f8d2490b35cb57a999a8866318fb
stanstaton <thatkidenzo@gmail.com>
2019-07-25 13:09:17 -0700

    fizzbuzz

--- forloops.js
+++ forloops.js
@@ -17,3 +17,15 @@ var busRouteEnds = 24
 for (var busroutes = 5; busroutes <= busRouteEnds; busroutes++) {
 	console.log(`This bus started at ${busroutes}:00`);
 }
+
+for (var i = 1; i <= 100; i++) {
+	if (i % 3 == 0 && i % 5 == 0) {
+		console.log("fizzbuzz");
+	} else if (i % 3 == 0) {
+		console.log("fizz");
+	} else if (i % 5 == 0) {
+		console.log("buzz");
+	} else {
+		console.log(i);
+	}
+}
